54574241e24832eda5696a38a916ddbe449c42d3
Sadi Zeynalov <sadi.zeynalov@cyberhull.com>
2023-10-26 13:31:29 +0300

    added version and fix bug

--- src/main.tsx
+++ src/main.tsx
@@ -1,48 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App/App.tsx'
-import { Provider } from 'react-redux'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import HomePage from './pages/HomePage/Homepage.tsx'
-import LoginPage from './pages/LoginPage/LoginPage.tsx'
-import BrowsePage from './pages/BrowsePage/BrowsePage.tsx'
-import store, { persistor } from './store/store'
-import './index.scss'
-import { PersistGate } from 'redux-persist/integration/react'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App.tsx";
+import { Provider } from "react-redux";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage.tsx";
+import LoginPage from "./pages/LoginPage/LoginPage.tsx";
+import BrowsePage from "./pages/BrowsePage/BrowsePage.tsx";
+import store, { persistor } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import "./index.scss";
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: (
-      <App>
-        <HomePage />
-      </App>
-    ),
-  },
-  {
-    path: 'login',
-    element: (
-      <App>
-        <LoginPage />
-      </App>
-    ),
-  },
-  {
-    path: 'browse',
-    element: (
-      <App>
-        <BrowsePage />
-      </App>
-    ),
-  },
-])
+    {
+        path: "/",
+        element: (
+            <App>
+                <HomePage />
+            </App>
+        ),
+    },
+    {
+        path: "login",
+        element: (
+            <App>
+                <LoginPage />
+            </App>
+        ),
+    },
+    {
+        path: "browse",
+        element: (
+            <App>
+                <BrowsePage />
+            </App>
+        ),
+    },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <RouterProvider router={router} />
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <RouterProvider router={router} />
+            </PersistGate>
+        </Provider>
+    </React.StrictMode>
+);
